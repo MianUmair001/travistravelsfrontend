5773fa02087ef18b2be046a04eeb1a8ffe9cb324
DESKTOP-F9SHR1L\Lenovo <amianumair@gmail.com>
2021-09-14 22:39:46 +0500

    firstCommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from "./components/footer";
+import Header from "./components/header";
+import Home1 from "./components/Home1";
+import Home2 from "./components/Home2_with_only_tours";
+import Home_SingleImage from "./components/Home_SingleImage";
+import RevSlider from "./components/RevSlider";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Header />
+      <RevSlider />
+      <Footer /> */}
+      {/* <Home1 /> */}
+      {/* <Home2 /> */}
+      <Home_SingleImage />
     </div>
   );
 }
